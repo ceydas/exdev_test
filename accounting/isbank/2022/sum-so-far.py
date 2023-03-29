5a76ae629af8a361dd237b141ff5c5895ab9bd14
Ceyda Sahin <128907343+brianhenderburg@users.noreply.github.com>
2023-03-29 22:35:11 +0300

    added functions, deleted a file

--- accounting/isbank/2022/sum-so-far.py
+++ accounting/isbank/2022/sum-so-far.py
@@ -1,5 +1,8 @@
 import random
+import math
 
+
+# ceydas : 2 functions
 def sum_so_far(numbers):
     # Initialize a variable to keep track of the sum-so-far
     sum_so_far = 0
@@ -22,3 +25,64 @@ def generate_name():
     name = "".join([chr(total + 64) for i in range(3)])
 
     return name
+
+
+# brianhenderburg: 4 functions
+def check_ascii_sum(string):
+    # Convert the string to a list of ASCII character values
+    ascii_values = [ord(char) for char in string]
+
+    # Sum up the ASCII character values
+    ascii_sum = sum(ascii_values)
+
+    # Divide the sum by 300
+    result = ascii_sum / 300
+
+    # Check if the result is smaller than 1
+    if result < 1:
+        return True
+    else:
+        return False
+
+
+def play_rps():
+    # Define the list of choices
+    choices = ["rock", "paper", "scissors"]
+
+    # Choose a random index from the list
+    index = random.randint(0, 2)
+
+    # Return the choice at the random index
+    return choices[index]
+
+
+def calculate_average(num1, num2, num3):
+    # Calculate the average of three numbers
+    average = (num1 + num2 + num3) / 3
+
+    # Round the average to 2 decimal places
+    rounded_average = round(average, 2)
+
+    # Return the rounded average
+    return rounded_average
+
+
+def calculate_integral(a, b, n):
+    # Define the function to integrate
+    def f(x):
+        return math.sin(x)
+
+    # Calculate the width of each subinterval
+    h = (b - a) / n
+
+    # Calculate the sum of the function values at the endpoints of each subinterval
+    sum = 0
+    for i in range(n):
+        xi = a + i * h
+        sum += f(xi) + f(xi + h)
+
+    # Multiply the sum by the width of each subinterval and divide by 2 to get the approximate integral
+    integral = (h / 2) * sum
+
+    # Return the approximate integral
+    return integral
