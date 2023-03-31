d2bf2c1c33830ff7ff0645f3d66b987f399fb453
Ceyda Sahin <128907343+brianhenderburg@users.noreply.github.com>
2023-03-31 14:00:20 +0300

    earnings and news

---
+++ accounting/yapikredi/2021/yk_2021.py
@@ -0,0 +1,10 @@
+
+def accounting_problems_2021():
+    problems = [
+        "YK's accounts payable increased by 20% in Q2 of 2021 due to unexpected supplier costs.",
+        "An accounting error resulted in YK overstating its revenue by $5 million in Q3 of 2021.",
+        "YK's CFO discovered discrepancies in the company's tax filings for 2020, leading to a $10 million penalty.",
+        "A cyber attack on YK's financial systems in January 2021 resulted in the loss of financial data and records.",
+        "YK's merger with a smaller tech company in 2021 resulted in complications with financial reporting and consolidation."
+    ]
+    return problems
