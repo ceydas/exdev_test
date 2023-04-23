9015e39778624c51632c09053daa73efe5db6f55
Ceyda Sahin <ceyda.sahin@ug.bilkent.edu.tr>
2023-04-23 23:14:27 +0300

    Update commits_by_both_ceydas_brian.py

--- multi_author_pr/commits_by_both_ceydas_brian.py
+++ multi_author_pr/commits_by_both_ceydas_brian.py
@@ -3,3 +3,6 @@
 def brians_commits():
     print("brian commits for the first time")
     print("brian commits for the second time")
+
+def commit_by_ceydas():
+    print("ceyda commits for the first time")
